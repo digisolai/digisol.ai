006ecc0163626aa6317832a70468b9f30295190e
Cameron Brown <cam.r.bown82@gmail.com>
2025-08-05 21:35:32 -0600

    Fix Render deployment: update WSGI to use minimal settings and add SECRET_KEY generator

--- backend/digisol_ai/wsgi.py
+++ backend/digisol_ai/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'digisol_ai.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'digisol_ai.settings_minimal')
 
 application = get_wsgi_application()
